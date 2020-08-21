1edab6c97fcec49df4a4927632f9b3a3bd263326
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-22 03:47:42 +0530

    added mongoose and setup a mongodb replica set on mongo atlas

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { Server } from 'http';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect(process.env.dbUrl || 'mongodb+srv://kaushikmdeo:kaushik123@frontm.zltir.mongodb.net/frontm?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const PORT = process.env.port || 3000;
 
