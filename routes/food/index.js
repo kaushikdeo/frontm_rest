7f82b1e3d859444b2536fbd527999efb8efd820e
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-22 05:24:52 +0530

    added sorting and filtering foods

--- routes/food/index.js
+++ routes/food/index.js
@@ -7,7 +7,16 @@ router.get('/', async (req, res, next) => {
         const limit = Number(req.query.limit) || 5;
         const startIndex = (page - 1) * limit;
         const endIndex = page * limit;
+        const sortBy = req.query.sortBy ? req.query.sortBy.toString() : 'itemName' ;
+        const orderBy = Number(req.query.orderBy) === 1 ? 1 : -1;
+        const filterString = req.query.filter ? req.query.filter.toString() : '';
         const results = {error: false};
+        const sortQuery = {
+            [sortBy]: orderBy,
+        };
+        const filterQuery = {
+            itemName: new RegExp(filterString, 'i')
+        }
         const foodsLength = await Food.countDocuments().exec();
         if (((page * limit) > foodsLength) && (page * limit) - foodsLength > limit) {
             return res.status(400).json({error: true, message: 'Invalid page'})
@@ -24,7 +33,7 @@ router.get('/', async (req, res, next) => {
                 limit,
             }
         }
-        const allFoods = await Food.find().limit(limit).skip(startIndex);
+        const allFoods = await Food.find(filterQuery).limit(limit).skip(startIndex).sort(sortQuery);
         if (allFoods) results.foods = allFoods;
         return res.status(200).json(results);
     } catch (error) {
