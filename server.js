f2d89b52eeff3df7201740e1856fd5f056d50c22
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-22 03:52:57 +0530

    setup root route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 const app = express();
+const router = express.Router();
 
 mongoose.connect(process.env.dbUrl || 'mongodb+srv://kaushikmdeo:kaushik123@frontm.zltir.mongodb.net/frontm?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -11,6 +12,9 @@ const PORT = process.env.port || 3000;
 const sever = Server(app);
 
 app.use(bodyParser.json());
+app.use(router);
+
+app.get('/', (req, res) => res.send('Welcome to FrontM'));
 
 sever.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
