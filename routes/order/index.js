84e984ede30f56aef20c35b9bbb8ff25ac820838
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-22 08:00:14 +0530

    fault tolerance bug

--- routes/order/index.js
+++ routes/order/index.js
@@ -12,36 +12,35 @@ router.post('/', async (req, res, next) => {
         return res.status(400).json({messgae: 'Invalid input fields'});
     }
     const session = await mongoose.startSession();
+    session.startTransaction();
     try {
-        await session.withTransaction(async () => {
-            const isValid = await Promise.all((req.body.map(async (foodItem) => {
-                const isFoodQuantity = await Food.findOne({
-                    _id: foodItem.foodItem,
-                    inventory: {$gte: foodItem.itemCount}
-                }).session(session);
-                console.log('isFoodQuantity', isFoodQuantity);
-                if (!isFoodQuantity) throw new Error('one of the food item out of stock');
-                await Food.findOneAndUpdate({
-                    _id: foodItem.foodItem,
-                    inventory: {$gte: foodItem.itemCount}
-                }, {
-                    $inc: {
-                        inventory: -foodItem.itemCount,
-                    }
-                })
-            })))
-            if (isValid) {
-                const order = new Order({
-                    orderItems: req.body,
-                }, {_id: true, session});
-                const savedOrder = await order.save();
-                await session.commitTransaction();
-                const totalTime = process.hrtime(req.queryStartTime);
-                return res.status(200).json({savedOrder, totalExecutionTime: `${totalTime[0]} s ${totalTime[1]/1000000} ms`});
-            }
-        })
+        const isValid = await Promise.all((req.body.map(async (foodItem) => {
+            const isFoodQuantity = await Food.findOne({
+                _id: foodItem.foodItem,
+                inventory: {$gte: foodItem.itemCount}
+            }).session(session);
+            console.log('isFoodQuantity', isFoodQuantity);
+            if (!isFoodQuantity) throw new Error('one of the food item out of stock');
+            await Food.findOneAndUpdate({
+                _id: foodItem.foodItem,
+                inventory: {$gte: foodItem.itemCount}
+            }, {
+                $inc: {
+                    inventory: -foodItem.itemCount,
+                }
+            })
+        })))
+        if (isValid) {
+            const order = new Order({
+                orderItems: req.body,
+            }, {_id: true, session});
+            const savedOrder = await order.save();
+            await session.commitTransaction();
+            const totalTime = process.hrtime(req.queryStartTime);
+            return res.status(200).json({savedOrder, totalExecutionTime: `${totalTime[0]} s ${totalTime[1]/1000000} ms`});
+        }
     } catch (error) {
-        console.log('I am caught', error);
+        console.log('I am caught');
         await session.abortTransaction();
         return res.status(500).json({message: 'Error occured while saving new food item'});
     } finally {
